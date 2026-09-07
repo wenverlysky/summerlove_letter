2a317900caf6ef58927dd76e2e26803ee64558d8
wenverlysky <weethara0209@gmail.com>
2026-09-07 21:58:44 +0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,12 @@
+// ฟังก์ชันหน่วงเวลาตัวอักษร
+function typeEffect(text, speed) {
+    let i = 0;
+    const screen = document.getElementById('lcd-screen');
+    screen.innerText = "";
+    
+    let interval = setInterval(() => {
+        screen.innerText += text.charAt(i);
+        i++;
+        if (i >= text.length) clearInterval(interval);
+    }, speed);
+}
